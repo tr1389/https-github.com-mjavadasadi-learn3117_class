e882f4d65e083074498f93c5604afb10e3158eba
mjavadasadi <asadij313@gmail.com>
2024-07-13 21:42:32 +0330

    tamrin DOM

--- contactScript.js
+++ contactScript.js
@@ -1,18 +1,34 @@
-document.getElementById("contactForm").addEventListener("submit",function (event) {
+document.getElementById("contactForm").addEventListener("submit", function (event) {
     let name = document.getElementById("fullname").value;
     let email = document.getElementById("email").value;
     let massage = document.getElementById("massage").value;
 
     if (!name || !email || !massage) {
       alert("لطفا تمامی مقادیر پر شود.");
-      event.preventDefult();
+      event.preventDefault();
     } else {
-      let emailPattern =
-        /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+      let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
       if (!emailPattern.test(email)) {
         alert("لطفا یک ایمیل معتبر وارد کنید");
-        event.preventDefult();
+        event.preventDefault();
       }
     }
   });
+
+
+
+
+
+
+
+
+
+
+let contactForm = document.getElementById("contactForm");
+let confirmationMassage = document.getElementById("confirmationMassage");
+
+contactForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  confirmationMassage.style.display='block';
+});
